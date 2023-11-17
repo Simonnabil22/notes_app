5241a9bf1eaf248cedfff282bfdafe59301d415f
Simonnabil22 <Simonnabil22@gmail.com>
2023-11-17 23:43:35 +0200

    text and icon

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ void main() {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark
       ),
