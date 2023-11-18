fab99d0a82d7ca76fe57f9063c9acf35f2a1a2e4
Simonnabil22 <Simonnabil22@gmail.com>
2023-11-18 17:05:49 +0200

    create note model

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,15 @@ import 'package:notes_app/views/notes_view.dart';
 void main() {
   runApp(const NotesApp());
 }
- class NotesApp extends StatelessWidget {
+
+class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins'
-      ),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
       home: const NotesView(),
     );
   }
