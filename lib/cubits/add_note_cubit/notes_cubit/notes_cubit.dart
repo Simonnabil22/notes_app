213d18391d1142817522325c69c333a520940bb7
Simonnabil22 <Simonnabil22@gmail.com>
2023-11-20 03:04:37 +0200

    display nores part one

--- lib/cubits/add_note_cubit/notes_cubit/notes_cubit.dart
+++ lib/cubits/add_note_cubit/notes_cubit/notes_cubit.dart
@@ -9,17 +9,19 @@ part 'notes_state.dart';
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
+
+List<NoteModel>? notes ;
 fetchAllNotes()async{
   
-    try {
+    
       var notesBox = Hive.box<NoteModel>(KNotesBox);
 
      
-    List<NoteModel>notes = notesBox.values.toList();
-       emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFailure(e.toString()));
-    }
+    notes = notesBox.values.toList();
+       
+     
+      
+    
   }
 }
 
